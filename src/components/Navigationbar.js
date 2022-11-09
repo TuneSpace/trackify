441208aecb4bedbe43dd3262670bcd4a8fc20bd7
Shelton.Seaborn <seabornranger@aol.com>
2022-11-09 09:00:11 -0800

    Updated Navbar to Reflect Project Name

--- src/components/Navigationbar.js
+++ src/components/Navigationbar.js
@@ -6,7 +6,7 @@ const Navigationbar = () => {
   return (
     <Navbar bg="light" expand="lg">
     <Container>
-      <Navbar.Brand href="#home">Notebook App</Navbar.Brand>
+      <Navbar.Brand href="#home">Trackify</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
