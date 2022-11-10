5d7d3690de882f3391c82d629ddbf5825a1d8716
seabornjr <110051557+seabornjr@users.noreply.github.com>
2022-11-10 09:11:00 -0800

    Feature/browse page (#2)
    * Added Browse Page Features
    
    * added functionality to results cards
    
    * Added comments to browse
    
    Co-authored-by: Shelton.Seaborn <seabornranger@aol.com>

--- src/Pages/Browse.js
+++ src/Pages/Browse.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import { Button, Container, Form, InputGroup } from 'react-bootstrap'
+import Results from '../components/Results'
 
-const Browse = () => {
+//Create Structure for the Browse Page
+const Browse = ({tracks}) => {
   return (
-    <div>Browse</div>
+    <div>
+        <Container fluid="md">
+            <InputGroup className="mb-3">
+            <Button variant="outline-secondary" id="button-addon1">
+            Search
+            </Button>
+            <Form.Control
+            aria-label="Example text with button addon"
+            aria-describedby="basic-addon1"
+            />
+            </InputGroup>
+        </Container>
+        <Container>
+            <Results tracks={tracks}/>
+        </Container>
+    </div>
   )
 }
 
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import { Button, Card, Container } from 'react-bootstrap'
 
-const Results = () => {
+const Results = ({tracks}) => {
+    console.log({tracks})
   return (
-    <div>Results</div>
+    <div>
+    <Container id="results-container">
+        {tracks.map(track => ( 
+                <div id='results-card'>
+                    <Card border="success" style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={track.data.albumOfTrack.coverArt.sources[0].url} />
+                    <Card.Body>
+                    <Card.Title>{track.data.name}</Card.Title>
+                    <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="success" onClick={`location.href=${track.data.albumOfTrack.sharingInfo.shareURL}`}>Show In Spotify</Button>
+                    </Card.Body>
+                     </Card>
+                 </div>
+            
+        ))} 
+       
+       <div id="player">
+        {/* insert spotify player here*/}
+       </div>
+    </Container>
+    </div>
   )
 }
 
