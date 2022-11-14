0931d49778e82c1b0d974cd58bddc4ff8ec89cb8
renem678 <108833934+renem678@users.noreply.github.com>
2022-11-14 11:06:21 -0600

    login/register (fix line 2 on both)

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+//s
 
 const Login = (props) => {
   const [email, setEmail] = useState("");
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+//gfdgfd
 
 const Register = (props) => {
   const [email, setEmail] = useState("");
