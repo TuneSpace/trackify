90b3a4a8f6977f0fb7bc79430a7e4e78fd796fab
renem678 <108833934+renem678@users.noreply.github.com>
2022-11-14 11:26:13 -0600

    Corrected login/register

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,12 +1,22 @@
 import { useState } from "react";
 
 const Register = (props) => {
-  const [email, setEmail] = useState("");
-  const [pass, setPass] = useState("");
-  const [username, setUsername] = useState("");
+  const [input, setInput] = useState({
+    email: "",
+    passcode: "",
+    username: "",
+    avatar: "",
+  });
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    console.log();
+  };
+
+  const handleChange = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+    console.log(input);
+    return input;
   };
 
   return (
@@ -16,28 +26,45 @@ const Register = (props) => {
         <label htmlFor="name">Username</label>
         <input
           type="name"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
+          onChange={handleChange}
           id="name"
+          value={input.username}
+          name="username"
           placeholder="Create Username"
         />
         <label htmlFor="Email">Email</label>
         <input
           type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
+          onChange={handleChange}
           id="email"
+          name="email"
           placeholder="youremail@gmail.com"
+          value={input.email}
         />
         <label htmlFor="password">Password</label>
         <input
           type="password"
-          value={pass}
-          onChange={(e) => setPass(e.target.value)}
+          onChange={handleChange}
           placeholder="your password"
           id="password"
+          value={input.passcode}
+          name="passcode"
         />
-        <button type="submit">Log In</button>
+        <button
+          type="submit"
+          onClick={() =>
+            fetch("http://localhost:8000/user", {
+              method: "POST",
+              mode: "cors",
+              headers: {
+                "Content-type": "application/json; charset=UTF-8",
+              },
+              body: JSON.stringify(input),
+            }).then(console.log(input))
+          }
+        >
+          Log In
+        </button>
       </form>
       <button className="link-btn" onClick={() => props.onFormSwitch("login")}>
         Already Have an Account? Login Here
